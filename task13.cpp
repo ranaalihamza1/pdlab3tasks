1d5f391b116ac247f5d0f2e312b4016148ba8c9f
Ali Hamza <144408584+ranaalihamza1@users.noreply.github.com>
2023-09-22 16:27:51 -0700

    Add files via upload

---
+++ task13.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+main()
+{ 	int sum,num;
+ 	sum=0;
+	cout<<"Enter a number: ";
+	cin>>num;
+	sum=sum+num;
+	cout<<"Enter a number: ";
+	cin>>num;
+	sum=sum+num;
+	cout<<"Enter a number: ";
+	cin>>num;
+	sum=sum+num;
+	cout<<"Enter a number: ";
+	cin>>num;
+	sum=sum+num;
+	cout<<"Enter a number: ";
+	cin>>num;
+	sum=sum+num;
+	cout<<sum;}
+	
+	
+	
